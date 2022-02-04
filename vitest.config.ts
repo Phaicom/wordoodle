a1866b491bd4c27a839435d79945777eaa44729e
phaicomm <rc@zrch.com>
2022-02-04 22:05:56 +0700

    feat: config vitest path

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,17 +5,8 @@ import { defineConfig } from 'vite'
 const r = (p: string) => resolve(__dirname, p)
 
 export const alias: AliasOptions = {
-  'unocss': r('./packages/unocss/src/'),
-  '@unocss/core': r('./packages/core/src/'),
-  '@unocss/vite': r('./packages/vite/src/'),
-  '@unocss/cli': r('./packages/cli/src/'),
-  '@unocss/inspector': r('./packages/inspector/node/'),
-  '@unocss/preset-mini': r('./packages/preset-mini/src/'),
-  '@unocss/preset-wind': r('./packages/preset-wind/src/'),
-  '@unocss/preset-uno': r('./packages/preset-uno/src/'),
-  '@unocss/preset-web-fonts': r('./packages/preset-web-fonts/src/'),
-  '@unocss/preset-attributify': r('./packages/preset-attributify/src/'),
-  '@unocss/preset-icons': r('./packages/preset-icons/src/'),
+  'wordoodle/vue': r('./packages/vue3/src/'),
+  '@wordoodle/core': r('./packages/core/src/'),
 }
 
 export default defineConfig({
