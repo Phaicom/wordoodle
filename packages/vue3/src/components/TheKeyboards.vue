02ff6bf471dd97f7b20994228f5ae7f1eccd6249
phaicomm <rc@zrch.com>
2022-02-05 23:43:16 +0700

    refactor: keyboard template

--- packages/vue3/src/components/TheKeyboards.vue
+++ packages/vue3/src/components/TheKeyboards.vue
@@ -1,37 +1,35 @@
 <script setup lang="ts">
 import { useWordoodle } from '@/hooks/useWordoodle'
 
-const { check, deleteWord, addWord, keysInput, resultSet, isReadyForCheck } = useWordoodle()
+const { check, deleteWord, addWord, isReadyForCheck } = useWordoodle()
 const keys = [['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'], ['', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ''], ['enter', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'del']]
 
-const onInput = (word: string) => {
-  if (word === '')
+const onClickKey = (key: string) => {
+  if (key === '')
     return
-  if (word === 'enter') {
+  if (key === 'enter') {
     if (isReadyForCheck.value)
       check()
     return
   }
-  if (word === 'del') {
+  if (key === 'del') {
     deleteWord()
     return
   }
 
-  addWord(word)
+  addWord(key)
 }
 </script>
 
 <template>
   <div class="flex flex-col p-2 gap-1">
-    {{ keysInput }}
-    {{ resultSet }}
     <div v-for="(line, i) in keys" :key="i" class="flex gap-1">
       <button
         v-for="key in line"
         :key="key"
         class="btn btn-primary h-14 flex flex-1 select-none p-0"
         :class="{ 'bg-transparent border-none btn-disabled': !key }"
-        @click="onInput(key)"
+        @click="onClickKey(key)"
       >
         {{ key }}
       </button>
